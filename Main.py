cf0af9c052e2b663ac9af22009ba585d5e3114a8
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-01-08 21:04:36 +0000

    install

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import json
 from rich import print_json
 import getpass
 from elevate import elevate
-
+from time import sleep
 
 elevate()
 
@@ -42,7 +42,9 @@ data = json.load(setup_type)
 
 
 if data["setup_type"] == 'full':
+    print_json(data=data)
     print('')
+    sleep(2)
     print(Colorate.Color(Colors.purple, ' Installing Spotify (No Ads)...', True))
     os.system('curl -L -O https://github.com/SpotX-CLI/SpotX-Win/releases/download/1.7/Install.bat')
     os.system(f'Install.bat')
@@ -51,19 +53,52 @@ if data["setup_type"] == 'full':
     os.system("powershell -command Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))")
 
     applications = '''
-
+    choco install visualstudiocode -y
+    choco install slack -y
+    choco install pandoc -y
+    
+    
+    choco install git -y
+    choco install github -y -ignore-checksums
+    
+     
+    choco install docker-desktop -y
+    choco install terraform -y
+    choco install chefdk -y
+    choco install packer -y
+    choco install vagrant -y
+    
+     
+    choco install kubernetes-cli -y
+    choco install kubernetes-helm -y
+    choco install minikube -y
+    choco install eksctl -y
+    
+     
+    choco install azure-data-studio -y
+    choco install sql-server-management-studio -y
+    
+    
+    choco install putty -y
+    choco install mremoteng -y
+    
+     
+    choco install azure-cli -y
+    choco install awscli -y
+    
+    
+    choco install python3 -y
+    choco install pip -y
+    
+    
+    pip install pre-commit
+    pip install terrascan
     
         
         
     '''
     os.system(applications)
     
-
-elif data["setup_type"] == 'essential':
-    print('')
-
-elif data["setup_type"] == 'custom':
-    print('')
 
 
 else:
